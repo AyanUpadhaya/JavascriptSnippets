3ac4628d2f76b08054fc44dc870ceb3ea1185f9e
Ayan Upadhaya <ayanu881@gmail.com>
2022-04-17 18:32:13 +0600

    Create obects.js

---
+++ obects.js
@@ -0,0 +1,16 @@
+//objects
+
+let person = {
+	name:'Ayan',
+	age:29,
+	country:'Bangladesh',
+	hobbies:['chess','music','programming'],
+	sayHi: function(){
+		console.log('Hello Friends');
+	}
+}
+
+
+console.log(person.name);
+console.log(person.hobbies);
+person.sayHi();
