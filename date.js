69546d9e37570631f2b7ae046656afb3580c10c2
Ayan Upadhaya <ayanu881@gmail.com>
2022-05-07 01:31:30 +0600

    date in js

---
+++ date.js
@@ -0,0 +1,15 @@
+
+//ternary operations
+const isPlaying = true;
+
+result = isPlaying ? "playing": "not playing";
+
+console.log(result);
+
+//date
+
+const date = new Date();
+
+const today = `Today: ${date.getFullYear()},${date.getMonth()+1},${date.getDate()}`;
+
+console.log(today);
